2775602ea45424380ab6cd54e1211ece8acc6576
jeroendedauw <jeroendedauw@gmail.com>
2013-08-26 04:24:40 +0200

    Update JSHint config

--- test/QueryEngine_test.js
+++ test/QueryEngine_test.js
@@ -3,14 +3,14 @@
 var QueryEngine = require('../lib/QueryEngine.js');
 
 exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(QueryEngine.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  },
+	setUp: function(done) {
+		// setup here
+		done();
+	},
+	'no args': function(test) {
+		test.expect(1);
+		// tests here
+		test.equal(QueryEngine.awesome(), 'awesome', 'should be awesome.');
+		test.done();
+	}
 };
